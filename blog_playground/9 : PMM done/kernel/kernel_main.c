3d5c9468d706cb720afffe73344b77c1a5a21650
1x31744 <koopahello@gmail.com>
2026-07-16 00:11:15 +0100

    finished pmm

---
+++ blog_playground/9 : PMM done/kernel/kernel_main.c
@@ -0,0 +1,26 @@
+#include "vga_text.h"
+#include "interrupts.h"
+#include "timer.h"
+#include "keyboard.h"
+#include "../memory/pmm.h"
+
+vga_text terminal;
+
+void kernel_main(void)
+{
+    volatile char* vga = (volatile char*)0xB8000;
+    
+    //signal that we have reached C
+    vga[0] = 'C';
+    vga[1] = 0x02;
+
+    vga_text_init(&terminal);
+    vga_text_writeline(&terminal, "Welcome to the lytlnybl kernel in protected mode");
+
+    idt_init();
+    timer_init(100);
+    keyboard_init();
+    init_pmm(); 
+
+    for (;;);
+}
---
+++ blog_playground/9 : PMM done/kernel/vga_text.h
@@ -0,0 +1,104 @@
+#ifndef VGA_TEXT_H
+#define VGA_TEXT_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef enum {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE,
+    VGA_COLOR_GREEN,
+    VGA_COLOR_CYAN,
+    VGA_COLOR_RED,
+    VGA_COLOR_MAGENTA,
+    VGA_COLOR_BROWN,
+    VGA_COLOR_LIGHT_GREY,
+    VGA_COLOR_DARK_GREY,
+    VGA_COLOR_LIGHT_BLUE,
+    VGA_COLOR_LIGHT_GREEN,
+    VGA_COLOR_LIGHT_CYAN,
+    VGA_COLOR_LIGHT_RED,
+    VGA_COLOR_LIGHT_MAGENTA,
+    VGA_COLOR_LIGHT_BROWN,
+    VGA_COLOR_WHITE
+} vga_color;
+
+typedef struct {
+    size_t row;
+    size_t column;
+
+    size_t width;
+    size_t height;
+
+    uint8_t color;
+
+    uint16_t* buffer;
+} vga_text;
+
+// init
+void vga_text_init(vga_text* terminal);
+
+//screen ops
+//
+void vga_text_clear(vga_text* terminal);
+
+/* cursor ops */
+void vga_text_set_cursor(
+    vga_text* terminal,
+    size_t row,
+    size_t column
+);
+
+/* Color ops */
+void vga_text_set_color(
+    vga_text* terminal,
+    vga_color f,
+    vga_color b
+);
+
+/* char out */
+void vga_text_putchar(
+    vga_text* terminal,
+    char c
+);
+
+/* string out */
+void vga_text_write(
+    vga_text* terminal,
+    const char* string
+);
+
+void vga_text_writeline(
+    vga_text* terminal,
+    const char* string
+);
+
+/* num out */
+void vga_text_write_dec(
+    vga_text* terminal,
+    uint32_t value 
+);
+
+void vga_text_write_hex(
+    vga_text* terminal,
+    uint32_t value
+);
+
+/* low level writing */
+
+void vga_text_put_entry_at(
+    vga_text* terminal,
+    char character,
+    uint8_t fcolor,
+    uint8_t bcolor,
+    size_t row,
+    size_t column
+);
+
+/* operations added in making input */
+
+void vga_text_backspace(vga_text* terminal);
+char vga_text_getchar(vga_text* terminal);
+
+
+#endif
