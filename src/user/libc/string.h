4281072f3160f4010cd1b5739e9037d490a132ee
1x31744 <koopahello@gmail.com>
2026-08-22 23:51:06 +0100

    finished libc

--- src/user/libc/string.h
+++ src/user/libc/string.h
@@ -6,7 +6,10 @@
 size_t strlen(const char *str);
 int strcmp(const char *str1, const char *str2);
 int strncmp(const char *str1, const char *str2, size_t n);
-char strcpy(char* dest, const char *src);
+char* strcpy(char* dest, const char *src);
+char* strcat(char* dest, const char *src);
+char* strchr(const char *str, int c);
+char* strstr(const char *text, const char *search);
 
 
 #endif
